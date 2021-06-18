6acb48cc8d7fe8f1d884d5113798ecf737b96ae3
twomat <86117521+twomat@users.noreply.github.com>
2021-06-18 17:49:28 +0300

    загружает файлы сайта

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$title = '';
+$name = '';
+$image = 'img/think.png';
+$email = '';
+$phone = '';
+
+require('components/header.php');
+require('components/about.php');
+require('components/status.php');
+require('components/photos.php');
+require('components/news.php');
+require('components/quotes.php');
+require('components/contacts.php');
+require('components/footer.php');
